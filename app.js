7e9dcd064cb9ee8c42c912ee5ad27562c5a051dd
daniel.chazan <dhjb.students@gmail.com>
2017-09-25 13:03:48 +0300

    Initial commit for Hello heroku Application

---
+++ app.js
@@ -0,0 +1,16 @@
+var app = require('express')();
+
+app.set("port" , process.env.PORT || 3000);
+
+app.get("/" , function(req,res) {
+    res.send("<h1>Hello Heroku World</h1>");
+    res.end("© <h4>From Snake Skill Project</h4>");
+});
+
+
+app.listen(app.get("port") , function() {
+    console.log("App started on port " + app.get("post"));
+});
+
+
+
